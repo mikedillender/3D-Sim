14f5b1046a65bef2855438e668429d33037b07ee
CuseCose <dillemic000@gmail.com>
2019-09-29 16:22:28 -0500

    ok i think i understand 3d lines now

--- src/Object.java
+++ src/Object.java
@@ -14,8 +14,16 @@ public class Object {
         //d=dist from source
         //x^2+y*2+z^2=r
         if (shape==0){
-
+            
         }
         return false;
+    }
+
+    public Vec3f getLocOnLine(Vec2f orient, Vec3f pos, float r) {
+        float r1=(float)(r*Math.cos(orient.y));
+        float x=(float)(r1*Math.cos(orient.x));
+        float y=(float)(r1*Math.sin(orient.x));
+        float z=(float)(r1*Math.tan(orient.y));
+        return new Vec3f(x+pos.x,y+pos.y,z+pos.z);
     }
 }
