b2041d7125499d9f18449fb72331b4090f4d7bca
CuseCose <dillemic000@gmail.com>
2019-09-29 17:30:40 -0500

    very basic, but it works

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main extends Applet implements Runnable, KeyListener {
     ArrayList<Object> objects=new ArrayList<>();
     Vec3f pos=new Vec3f(0,0,0);
     Vec2f orient=new Vec2f(0,0);
+    float orientfromcenter=0;
     //GRAPHICS OBJECTS
     private Thread thread;
     Graphics gfx;
@@ -29,7 +30,8 @@ public class Main extends Applet implements Runnable, KeyListener {
         this.addKeyListener(this);
         img=createImage(WIDTH,HEIGHT);
         gfx=img.getGraphics();
-        objects.add(new Object(0,new Vec3f(0,0,0),new Vec3f(0,0,0)));
+        objects.add(new Object(0,new Vec3f(50,0,0),new Vec3f(0,0,0)));
+        objects.add(new Object(0,new Vec3f(50,20,0),new Vec3f(0,0,0)));
         thread=new Thread(this);
         thread.start();
     }
@@ -41,14 +43,31 @@ public class Main extends Applet implements Runnable, KeyListener {
         int pSize=3;
         int pw=WIDTH/pSize;
         int ph=HEIGHT/pSize;
-
+        //float fovx=3.14f*2/3;
+        //float fovy=3.14f/2;
+        int cx=pw/2;
+        int cy=ph/2;
+        int lensdist=cy;
+        for (int x=0; x<pw; x++){
+            float xor=(float)(Math.atan2(x-cx,lensdist));
+            for(int y=0; y<ph; y++){
+                float yor=(float)(Math.atan2(y-cy,lensdist));
+                gfx.setColor(getColorInDir(xor,yor));
+                gfx.fillRect(pSize*x,pSize*y,pSize,pSize);
+            }
+        }
 
         //FINAL
         g.drawImage(img,0,0,this);
     }
 
-    public Color getColorAtPixel(int x, int y){
-
+    public Color getColorInDir(float xor,float yor){
+        Vec2f o1=new Vec2f(orient.x+xor,orient.y+yor);
+        for (Object o : objects){
+            if (o.doesLineCross(o1,pos)){
+                return Color.BLUE;
+            }
+        }
         return Color.WHITE;
     }
 
@@ -69,7 +88,23 @@ public class Main extends Applet implements Runnable, KeyListener {
 
     //INPUT
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode()==KeyEvent.VK_RIGHT){
+            orient.x+=.2f;
+        }else if (e.getKeyCode()==KeyEvent.VK_LEFT){
+            orient.x-=.2f;
+        }if (e.getKeyCode()==KeyEvent.VK_UP){
+            orient.y+=.2f;
+        }else if (e.getKeyCode()==KeyEvent.VK_DOWN){
+            orient.y-=.2f;
+        }if (e.getKeyCode()==KeyEvent.VK_A){
+            pos.x+=.2f;
+        }else if (e.getKeyCode()==KeyEvent.VK_D){
+            pos.x-=.2f;
+        }if (e.getKeyCode()==KeyEvent.VK_W){
+            pos.y+=.2f;
+        }else if (e.getKeyCode()==KeyEvent.VK_S){
+            pos.y-=.2f;
+        }
     }
     public void keyReleased(KeyEvent e) {
 
--- src/Object.java
+++ src/Object.java
@@ -5,6 +5,8 @@ public class Object {
     int shape=0;//sphere
     Vec3f loc;
     Vec3f vel;
+    float rad=2;
+
     public Object(int shape, Vec3f loc, Vec3f vel){
         this.vel=vel;
         this.shape=shape;
@@ -14,11 +16,44 @@ public class Object {
         //d=dist from source
         //x^2+y*2+z^2=r
         if (shape==0){
-            
+            Vec3f dv=getDeltaVecBetween(pos,loc);
+            Vec2f dor=getDeltaOrient(dv);
+            //System.out.println("looking in "+orient+", at "+dor);
+            float odif=getOrientDif(orient,dor);
+            float arclength=(odif*getDistOfDelta(dv));
+            if(arclength<5){
+                return true;
+            }
+            //float cr=getDistOfDelta(dv);
         }
         return false;
     }
-
+    public float getDistOfDelta(Vec3f dp){
+        return (float)(Math.sqrt((dp.x*dp.x)+(dp.y*dp.y)+(dp.z*dp.z)));
+    }
+    public Vec2f getDeltaOrient(Vec3f dp){
+        float r1=(dp.x*dp.x)+(dp.y*dp.y);
+        float th1=(float)Math.atan2(dp.z,r1);
+        float th0=(float)Math.atan2(dp.y,dp.x);
+        return new Vec2f(th0,th1);
+    }
+    public Vec3f getDeltaVecBetween(Vec3f p1, Vec3f p2){
+        return new Vec3f(p2.x-p1.x,p2.y-p1.y,p2.z-p1.z);
+    }
+    public float getOrientDif(Vec2f o1, Vec2f o2){
+        float d0=getAngDif(o1.x,o2.x);
+        float d1=getAngDif(o1.y,o2.y);
+        return (float)Math.sqrt((d0*d0)+(d1*d1));
+    }
+    public float getAngDif(float a1, float a2){
+        float ad=a1-a2;
+        if(ad<-3.15){
+            ad+=6.28;
+        }else if(ad>3.15){
+            ad-=6.28;
+        }
+        return ad;
+    }
     public Vec3f getLocOnLine(Vec2f orient, Vec3f pos, float r) {
         float r1=(float)(r*Math.cos(orient.y));
         float x=(float)(r1*Math.cos(orient.x));
